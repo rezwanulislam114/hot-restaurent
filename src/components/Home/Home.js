45cfa7b395b16cb9a5479f115c3830c55decc604
rezwanulislam114 <rezwanulislam114@gmail.com>
2021-10-15 23:43:39 +0600

    file mod

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Foods from '../Foods/Foods';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Food from '../Food/Food';
+import './Home.css'
 
 const Home = () => {
+
+    const [menu, setMenu] = useState([]);
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        fetch('./menu.json')
+            .then(res => res.json())
+            .then(data => {
+                setMenu(data)
+                setItems(data)
+            })
+    }, [])
+
+    const filterItem = catagory => {
+        const updatedItems = menu.filter(item => {
+            return item.catagory === catagory
+        })
+        setItems(updatedItems);
+    }
+
     return (
-        <div>
-            <Banner></Banner>
-            <Foods></Foods>
-        </div>
+        <>
+            <div className="banner">
+                <h1>Best Food Waiting For You.</h1>
+                <div className="search-box">
+                    <input type="text" />
+                    <button>Search</button>
+                </div>
+            </div>
+            <Container>
+                <div className="text-center">
+                    <button onClick={() => filterItem('breakfast')} className="menu-btn m-4">Breakfast</button>
+                    <button onClick={() => filterItem('lunch')} className="menu-btn m-4">Lunch</button>
+                    <button onClick={() => filterItem('dinner')} className="menu-btn m-4">Dinner</button>
+                    <button onClick={() => setItems(menu)} className="menu-btn m-4">All Items</button>
+                </div>
+                <div className="food-container">
+                    {
+                        items.map(item => <Food key={item.id} details={item}></Food>)
+                    }
+                </div>
+            </Container>
+        </>
     );
 };
 
